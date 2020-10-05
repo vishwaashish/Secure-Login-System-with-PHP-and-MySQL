2e0b7d0e5202d025c35825d828856e19b12de093
Ashishkumar Vishwakarma <vishwakarmaashish165@gmail.com>
2020-10-05 12:40:57 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,7 @@
 <?php
-$connect= mysqli_connect("localhost", "root", "", "testing");
-// Check connection
-if($connect === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-
-
-
+    $connect= mysqli_connect("localhost", "root", "", "loginsystem");
+    // Check connection
+    if($connect === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
 ?>
